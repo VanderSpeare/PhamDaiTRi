297d5508982e0a31ebc3e3d0b2c252286aacc42b
VanderSpeare <125333024+VanderSpeare@users.noreply.github.com>
2023-03-23 10:56:07 +0900

    Add files via upload

---
+++ Xedulich.java
@@ -0,0 +1,92 @@
+package Xedulich;
+import java.time.LocalTime
+import java.util.Date;
+import java.util.Scanner;
+
+public class Xedulich {
+    public void masotuyen;
+
+    public void setMasotuyen() {
+        return masotuyen
+    }
+
+    public void Hotentaixe;
+
+    public void setHotentaixe() {
+        return Hotentaixe
+    }
+
+    public double doanhthu;
+
+    public void getdoanhthu() {
+        return doanhthu
+    }
+
+    public xedulich() {
+    }
+
+    public xedulich(void masotuyen, void Hotentaixe, double doanhthu) {
+        this.masotuyen = new masotuyen;
+        this.Hotentaixe = new Hotentaixe;
+    }
+}
+            public class xenoithanh extends Xedulich{
+            public void Masotuyen(){
+              System.out.println("Biensoxe");
+                public double getDoanhthu() {
+                    return doanhthu;
+                }
+
+                public void setdoanhthu(double doanhthu) {
+                    this.doanhthu = doanhthu;
+                }
+                public void Hotentaix(){
+                    System.out.println("Ten nguoi lai xe: ")
+            public float sotuyen;
+            public float Kmdiduoc;
+            public string Kmdiduoc() {
+                return Kmdiduoc;
+            }
+
+            public void setKmdiduoc(toString() Kmdiduoc) {
+                this.Kmdiduoc = Kmdiduoc;
+            }
+
+                    public class xengoaithanh extends Xedulich{
+                        public void Masotuyen(){
+                            System.out.println("Biensoxe");
+                            public double getDoanhthu() {
+                                return doanhthu;
+                            }
+
+                            public void setdoanhthu(double doanhthu) {
+                                this.doanhthu = doanhthu;
+                            }
+                            public void Hotentaix(){
+                                System.out.println("Ten nguoi lai xe: ")
+                                public float noiden;
+                                public float songaydiduoc;
+                                public Date songaydiduoc() {
+                                    return songaydiduoc;
+                                }
+
+                                public void setsongaydiduoc(Date songaydiduoc) {
+                                    this.songaydiduoc = songaydiduoc;
+                                }
+
+        public void main(String[] args) {
+            Scanner n = new Scanner(System.in);
+            Xedulich xedulich=new Xedulich();
+            Xedulich.getmasotuyen();
+            Xedulich.getHotentaixe();
+            xenoithanh nt = new xenoithanh();
+            nt.getdoanhthu();
+            nt.getsotuyen();
+            nt.getKmdiduoc();
+            xengoaithanh ngt = new xengoaithanh();
+            ngt.getdoanhthu();
+            ngt.getsongaydiduoc;
+            ngt.getnoiden;
+        }
+    }
+}
